6dd10f6ee9eb714199e1eb36174e4a518aa970ca
MJebran <jebran.mustafa@gmail.com>
2024-11-11 23:36:17 -0700

    kube

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,21 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       "/api": "http://localhost:7070",//if I need to do locally 5150, and 7070 for kube
+//     },
+//   },
+// });
+
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": "http://localhost:7070",//if I need to do locally 5150, and 7070 for kube
-    },
-  },
-});
+})

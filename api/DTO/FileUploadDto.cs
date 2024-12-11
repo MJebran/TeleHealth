52498aaed9f2b00f0ab77a2fdd2a393b5b079845
MJebran <jebran.mustafa@gmail.com>
2024-12-10 22:54:25 -0700

    linter

--- api/DTO/FileUploadDto.cs
+++ api/DTO/FileUploadDto.cs
@@ -1,3 +1,15 @@
+// using Microsoft.AspNetCore.Mvc;
+
+// public class FileUploadDto
+// {
+//     [FromForm(Name = "file")]
+//     public required IFormFile File { get; set; }
+
+//     [FromForm(Name = "caseId")]
+//     public required int CaseId { get; set; }
+// }
+
+
 using Microsoft.AspNetCore.Mvc;
 
 public class FileUploadDto
@@ -8,3 +20,5 @@ public class FileUploadDto
     [FromForm(Name = "caseId")]
     public int CaseId { get; set; }
 }
+
+
